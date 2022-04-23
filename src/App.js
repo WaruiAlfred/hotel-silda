ed71a7322f8d84246cd9686d8f9bb099f19bda30
WaruiAlfred <alfredwarui17@gmail.com>
2022-04-24 02:59:07 +0300

    add basic app files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import { Fragment } from "react";
+import "./App.scss";
+import Container from "./components/Container";
+
+function App() {
+  return (
+    <Fragment>
+      <Container />
+    </Fragment>
+  );
+}
+
+export default App;
