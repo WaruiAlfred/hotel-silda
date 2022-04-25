46981a2134e5215ebd3ef600ccdff6112bd7e6cd
WaruiAlfred <alfredwarui17@gmail.com>
2022-04-25 16:17:09 +0300

    setup firebase app and redux store

--- src/index.js
+++ src/index.js
@@ -4,12 +4,29 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { initializeApp } from "firebase/app";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDCcDz6R8tt589aNzMEqE2w28DEnMSaEy8",
+  authDomain: "hotel-silda.firebaseapp.com",
+  projectId: "hotel-silda",
+  storageBucket: "hotel-silda.appspot.com",
+  messagingSenderId: "278464398662",
+  appId: "1:278464398662:web:640b477ad4ce32ac111147",
+  measurementId: "G-YP1CZHXYX9",
+};
+
+initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
