d1f274b531e274a9423361523fc1096f64e0eb10
WaruiAlfred <alfredwarui17@gmail.com>
2022-04-25 16:17:56 +0300

    create formik form inputs helper components

---
+++ src/components/helper-components/FormikFormInputs.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { useField } from "formik";
+
+const TextInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+export const SelectInput = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <select {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+export default TextInput;
