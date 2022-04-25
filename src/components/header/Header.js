6bae9ef4cbb34da994dd88d33a715a3598a5f46b
WaruiAlfred <alfredwarui17@gmail.com>
2022-04-26 00:42:58 +0300

    make user nav dynamic according to current users

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,14 +1,49 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import styles from "../../styles/components/header/Header.module.scss";
 import logo from "../../assets/img/logo.png";
 import UseSvg from "../helper-components/UseSvg";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { getAuth, signOut } from "firebase/auth";
+import BookRoom from "./BookRoom";
 
 function Header() {
   const [userMenuVisibility, setUserMenuVisibility] = useState(false);
+  const user = useSelector((state) => state.auth.user);
 
   const userMenuHandler = () => {
     setUserMenuVisibility((prevState) => !prevState);
   };
+
+  const logout = () => {
+    const auth = getAuth();
+    signOut(auth)
+      .then(() => {
+        // Sign-out successful.
+      })
+      .catch((error) => {
+        // An error happened.
+      });
+  };
+
+  let userOptionsLinks = (
+    <Link to="auth" className={styles["user__options-link"]}>
+      Login / Sign In
+    </Link>
+  );
+
+  if (user) {
+    userOptionsLinks = (
+      <Fragment>
+        <Link to="/" className={styles["user__options-link"]}>
+          Profile
+        </Link>
+        <Link to="/" onClick={logout} className={styles["user__options-link"]}>
+          Logout
+        </Link>
+      </Fragment>
+    );
+  }
 
   return (
     <header className={styles.header}>
@@ -21,14 +56,13 @@ function Header() {
         </button>
         {userMenuVisibility && (
           <div className={styles["user__options"]}>
-            <h5>New User</h5>
-            <button>Profile</button>
-            <button>Login / Sign In</button>
-            <button>Logout</button>
+            <h5>{user ? user.email : "New User"}</h5>
+            {userOptionsLinks}
           </div>
         )}
       </div>
       <h1 className={styles.title}>Hotel Silda</h1>
+      <BookRoom/>
     </header>
   );
 }
