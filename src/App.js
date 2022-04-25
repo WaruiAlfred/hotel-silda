6e2113dfd3aee13a5222dbd1576210ae039622da
WaruiAlfred <alfredwarui17@gmail.com>
2022-04-25 16:16:46 +0300

    configure routes,firebase and redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
-import { Fragment } from "react";
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.scss";
-import Container from "./components/Container";
+import HomePage from "./pages/HomePage";
+import AuthPage from "./pages/AuthPage";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useSelector, useDispatch } from "react-redux";
+import { authActions } from "./store/slices/authSlice";
 
 function App() {
+  const user = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const getCurrentUser = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in
+        const currentUser = {
+          accessToken: user.accessToken,
+          email: user.email,
+          phoneNumber: user.phoneNumber,
+          photoURL: user.photoURL,
+          reloadUserInfo: user.reloadUserInfo,
+          stsTokenManager: {
+            accessToken: user.stsTokenManager.accessToken,
+            expirationTime: user.stsTokenManager.expirationTime,
+            refreshToken: user.stsTokenManager.refreshToken,
+            isExpired: user.stsTokenManager.isExpired,
+          },
+          uid: user.uid,
+        };
+
+        dispatch(authActions.setUser(currentUser));
+      } else {
+        // User is signed out
+        dispatch(authActions.setUser(null));
+      }
+    });
+
+    return () => {
+      getCurrentUser();
+    };
+  }, [dispatch]);
+  console.log(user);
+
   return (
-    <Fragment>
-      <Container />
-    </Fragment>
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="auth" element={<AuthPage />} />
+    </Routes>
   );
 }
 
